d4c1e290190b971954e2025ab5bce6711bd57251
New-Kensyu <new-kensyu@jpn.meitec-grp.local>
2022-10-11 15:16:05 +0900

    enter funtion added

--- kaiketsu.java
+++ kaiketsu.java
@@ -1,10 +1,25 @@
 package 二次方程式;
 
+import java.util.Scanner;
+
 public class kaiketsu {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// ax2+bx+c=0 (a≠0)
+		// a,b,c enter
+		float a, b, c, x1, x2;
+		float delta;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("a =: ");
+		a = sc.nextFloat();
+		System.out.println("b =: ");
+		b = sc.nextFloat();
+		System.out.println("c =: ");
+		c = sc.nextFloat();
 
+		delta = b * b - 4 * a * c;
+
+		System.out.println(delta);
 	}
 
 }
