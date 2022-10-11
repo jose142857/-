8b184a4f084403c4f6df90bf1fdb426d3dd44c14
New-Kensyu <new-kensyu@jpn.meitec-grp.local>
2022-10-11 16:30:30 +0900

    解を出して終了

--- kaiketsu.java
+++ kaiketsu.java
@@ -8,18 +8,29 @@ public class kaiketsu {
 		// ax2+bx+c=0 (a≠0)
 		// a,b,c enter
 		float a, b, c, x1, x2;
-		float delta;
+		double delta;
+		double sqrtdelta ;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("a =: ");
+		System.out.print("a =: ");
 		a = sc.nextFloat();
-		System.out.println("b =: ");
+		System.out.print("b =: ");
 		b = sc.nextFloat();
-		System.out.println("c =: ");
+		System.out.print("c =: ");
 		c = sc.nextFloat();
 
 		delta = b * b - 4 * a * c;
+		sqrtdelta = Math.sqrt(delta);
 
-		System.out.println(delta);
+//		System.out.println(delta);
+		if (delta<0){
+			System.out.println("解がゼロ");
+
+		}else if (delta==0){
+			System.out.println("x1 = x2 = "+ -b/2*a);
+		}else {
+			System.out.println("x1 = "+ (-b +sqrtdelta)/2*a);
+			System.out.println("x2 = "+ (-b -sqrtdelta)/2*a);
+		}
 	}
 
 }
